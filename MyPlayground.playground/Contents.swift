4b61498e5d49ee06bfcb1aaf548f58279022642c
Apple <apple@Apples-iMac.local>
2017-11-06 16:45:16 +0530

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Raja"
+
+var str1 : String = ""
+
+str1 = ""
+
+let constantVar = 10
+
+let const1 = constantVar
+
+var const2 = "constantVar"
+
+print("The stored value: \(const2), \(const1) , \(str)")
+
+var green,blue,red,yellow:String
+
+green = "10"
+
+
+print("hello"); print("test");
+
+
+
+
